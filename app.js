2155583ab1aaaf19ad0e5399dbbbb5bd37329044
AlokPandey123 <128793825+AlokPandey123@users.noreply.github.com>
2023-11-07 14:22:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Third-Party
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true,
-  });
+  })
+);
 app.use(morgan('dev'));
 app.use(cookieParser());
 
